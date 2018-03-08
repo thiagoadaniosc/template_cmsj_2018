96992b309a7f939cfec30dec0766e1f1b33c23ff
thiagoadaniosc <thiagoadaniosc@gmail.com>
2018-03-08 20:53:01 +0000

    Correção links JS

--- footer.php
+++ footer.php
@@ -5,11 +5,11 @@
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
     crossorigin="anonymous"></script>
-<script src="<?= TEMPLATE_URI ?> /js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?= TEMPLATE_URI ?>/js/bootstrap.min.js" type="text/javascript"></script>
 
 <script src='https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/dataTables.bootstrap4.min.js'></script>
-<script src="<?= TEMPLATE_URI ?> /js/script.js"></script>
+<script src="<?= TEMPLATE_URI ?>/js/script.js"></script>
 
 <?php wp_footer() ?>
 </body>
